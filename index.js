9dde89e31afec93c6d28b35fe466e67f907d047f
mehedi-hasan02 <mehedisani02@gmail.com>
2024-06-03 19:55:13 +0600

    implement meal update and review api route

--- index.js
+++ index.js
@@ -12,49 +12,89 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fvwg0tw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
-  async function run() {
-    try {
-        const mealCollection = client.db('dormMasterDB').collection('meals');
-
-        //meal related api
-
-        app.post('/meal', async(req,res)=>{
-            const item = req.body;
-            const result = await mealCollection.insertOne(item);
-            res.send(result);
-        });
-
-        app.get('/meal', async(req,res)=>{
-            const result = await mealCollection.find().toArray();
-            res.send(result);
-        });
-
-        app.get('/meal/:id', async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
-            const result = await mealCollection.findOne(query);
-            res.send(result);
-        })
-      
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  run().catch(console.dir);
+});
 
-app.get('/', (req,res)=>{
-    res.send('Check your meal')
+async function run() {
+  try {
+    const mealCollection = client.db('dormMasterDB').collection('meals');
+    const reviewCollection = client.db('dormMasterDB').collection('reviews');
+
+    //meal related api
+
+    app.post('/meal', async (req, res) => {
+      const item = req.body;
+      const result = await mealCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.get('/meal', async (req, res) => {
+      const result = await mealCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/meal/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mealCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch('/meal/:id', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          title: item.title,
+          category: item.category,
+          image: item.image,
+          price: item.price,
+          ingredients: item.ingredients,
+          rating: item.rating,
+          like: item.like,
+          description: item.description
+        }
+      }
+
+      const result = await mealCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //review related api
+    app.post('/review', async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    })
+
+    app.get('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { mealId: id };
+      const result = await reviewCollection.find(filter).toArray();
+      res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Check your meal')
 })
 
-app.listen(port, ()=>{
-    console.log('Port id running on', port);
+app.listen(port, () => {
+  console.log('Port id running on', port);
 })
